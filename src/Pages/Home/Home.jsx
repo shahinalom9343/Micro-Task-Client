357b6c75b610e8d0071c77a6143696096153773b
shahinalom9343 <cuet.shahin.eee12@gmail.com>
2024-06-01 20:17:57 +0600

    create testimonials and top earner section with fake data

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Hero from "../../Components/Hero";
+import Testimonials from "../../Components/Testimonials";
+import TopEarners from "../../Components/TopEarners";
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       </Helmet>
       <div>
         <Hero></Hero>
+        <TopEarners></TopEarners>
+        <Testimonials></Testimonials>
       </div>
     </div>
   );
